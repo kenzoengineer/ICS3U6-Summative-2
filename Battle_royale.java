491290c226d3e662d75506db154bd75c4d61c720
Ken Jiang <073686289@gapps.yrdsb.ca>
2019-03-21 23:41:44 -0400

    Update Battle_royale.java

--- Battle_royale.java
+++ Battle_royale.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package battle_royale;
-
-/**
- *
- * @author kenj8
- */
 
 import java.util.Scanner;
 import java.io.File;
+import java.util.Arrays;
 public class Battle_royale {
     /**
-     * Prints the entire array
+     * prints the entire array
      * @param map the 2d array of the map
      * @param size the size of the 2d array
      */
@@ -28,7 +19,7 @@ public class Battle_royale {
     }
     
     /**
-     * Helper method to find the location of "P"
+     * helper method to find the location of "P"
      * @param map the 2d array of the map
      * @param size the size of the 2d array
      * @return the x and y coordinates of "P"
@@ -46,7 +37,7 @@ public class Battle_royale {
     }
     
     /**
-     * Shortest distance from the center to "P"
+     * shortest distance from the center to "P"
      * @param map the size of the map
      * @param x the current x value of the path
      * @param y the current y value of the path
@@ -88,8 +79,74 @@ public class Battle_royale {
         }
     }
     
+    /**
+     * checks if the index has already been visited
+     * @param x [x][] index
+     * @param y [][y] index
+     * @param isVisited 2d boolean array the size of the map, indicating whether the index was visited
+     * @return true or false depending on if it is visited
+     */
+    public static boolean checkVisit(int x, int y, boolean[][] isVisited) {
+        return isVisited[x][y];
+    }
+    
+    /**
+     * sets the path to a series of "V"s
+     * @param map the 2d array of the map
+     * @param path the string containing the coordinates of the path
+     */
+    public static void setV(String[][] map, String path) {
+        for (int i = 0; i < path.length() - 1; i += 4) {
+            int x = Integer.parseInt(path.substring(i,i+1));
+            int y = Integer.parseInt(path.substring(i+2,i+3));
+            map[x][y] = "V";
+        }
+    }
+    
+    /**
+     * recursively finds all legal paths from center to the player without taking into account loot
+     * @param x the current x index
+     * @param y the current y index
+     * @param map the 2d array of the map
+     * @param isVisited a 2d boolean array which holds values whether the index has been visited
+     * @param size the lengthwise size of the map
+     * @param path a string which holds the path taken
+     * @param max the maximum number of moves possible for the player to make, before the storm makes it impossible (floor(size/2))
+     */
+    public static void opPath(int x, int y, String[][] map, boolean[][] isVisited, int size, String path, int max) {
+        if (map[x][y].equals("P")) { // prints anytime it finds a path of length 5 without checking if it actually leads to the player
+            System.out.println(path);
+            System.out.println("");
+            //setV(map,path);
+        } else if (max > -1) {
+            path += x + "." + y + " ";
+            max--;
+            isVisited[x][y] = true;
+            if (x + 1 < size && !isVisited[x+1][y]) {
+                opPath(x + 1, y, map, isVisited, size, path, max);
+            }
+            if (x - 1 >= 0 && !isVisited[x-1][y]) {
+                opPath(x - 1, y, map, isVisited, size, path, max);
+            }
+            if (y + 1 < size && !isVisited[x][y+1]) {
+                opPath(x, y + 1, map, isVisited, size, path, max);
+            }
+            if (y - 1 >= 0 && !isVisited[x][y-1]) {
+                opPath(x, y - 1, map, isVisited, size, path, max);
+            }
+        }
+    }
+    
     public static void main(String[] args) throws Exception{
         int size = 11;
+        int max = size/2;
+        boolean[][] isVisited = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                isVisited[i][j] = false;
+            }
+        }
+        
         File file = new File("map.txt");
         Scanner fileIn = new Scanner(file);
         String[][] map = new String[size][size];
@@ -103,6 +160,8 @@ public class Battle_royale {
         System.out.println();
         System.out.println("The player collected " + path(map, 5, 5, size) + " piece(s) of loot");
         printA(map, size);
+        System.out.println("\n");
+        System.out.println("P is at " + Arrays.toString(findP(map,size)));
+        opPath(5,5,map,isVisited,size,"",max);
     }
-    
 }
